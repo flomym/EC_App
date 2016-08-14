15783f62fecc2bdc92638772db52443f32afff54
motya <i13moriwkai@gmail.com>
2016-08-14 17:29:25 +0900

    APIの最後まで

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+protect_from_forgery with: :exception, unless: :valid_api_request?
+
+  private
+
+  def valid_api_request?
+    # FIXME need authentication for realworld application
+    # request.format.json?
+    true
+  end
 end
